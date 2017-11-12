b11a977f16e3a8f276aa755777eceed136aef88c
Gilles Zunino <GillesZunino@users.noreply.github.com>
2017-11-11 16:00:09 -0800

    Add installation script - Add installation instructions to README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotfiles
 Personal dotfiles
+
+### WSL Configuration
+Run the following to configure WSL from scratch...
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/gilleszunino/dotfiles/wsl/configure.sh)"
+```
---
+++ configure.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Configure apt-get repositories
+echo "Updating apt-get repositor list..."
+sudo add-apt-repository ppa:git-core/ppa
+
+# Update pkg lists
+echo "Updating package lists..."
+sudo apt-get update
+
+# zsh install
+echo ''
+echo "Installing zsh..."
+echo ''
+sudo apt install zsh -y
+
+# git install
+echo ''
+echo "Installing git and bash-completion..."
+sudo apt-get install git bash-completion -y
+
+echo ''
+echo "Configuring git-completion..."
+GIT_VERSION=`git --version | awk '{print $3}'`
+URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
+echo ''
+echo "Downloading git-completion for git version: $GIT_VERSION..."
+if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
+	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
+fi
+
+# oh-my-zsh install
+echo ''
+echo "Installing oh-my-zsh..."
+echo ''
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# oh-my-zsh plugin install
+echo ''
+echo "Installing oh-my-zsh plugins..."
+echo ''
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+# powerlevel9k install
+echo ''
+echo "Installing powerlevel9k..."
+echo ''
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Bash color scheme
+echo ''
+echo "Installing solarized dark WSL color scheme..."
+echo ''
+wget https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
+mv dircolors.256dark .dircolors
+
+# vimrc vundle install
+echo ''
+echo "Installing vundle..."
+echo ''
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Pathogen install
+echo ''
+echo "Installing Pathogen..."
+echo ''
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Nerdtree for vim install
+echo ''
+echo "Installing Nerdtree for Vim..."
+echo ''
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# Vim color scheme install
+echo ''
+echo "Installing vim wombat color scheme..."
+echo ''
+git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
+mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
+
+
+
+# Set default shell to zsh
+echo ''
+read -p "Do you want to change your default shell? y/n" -n 1 -r
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Setting zsh as default shell..."
+    chsh -s $(which zsh); exit 0
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully set your default shell to zsh..."
+    else
+        echo "Default shell not set successfully..." >&2
+fi
+else 
+    echo "You chose not to configure zsh as default shell"
+fi
+echo ''
+echo '	Done'
