0b4dd455071598b239c9686de0d817c2394f8568
Gilles Zunino <gzunino@hotmail.com>
2020-11-28 18:50:15 -0800

    Make installation script idempotent - Set defults for git - MIscellanous fixes

--- configure.sh
+++ configure.sh
@@ -9,6 +9,11 @@ sudo add-apt-repository -y ppa:jonathonf/vim
 echo "Updating package lists..."
 sudo apt-get update
 
+# Upgrade all the things
+echo ''
+echo "Upgrade all apt packages and prune unused ones..."
+sudo apt-get full-upgrade -y && sudo apt-get autoremove -y
+
 # zsh install
 echo ''
 echo "Installing zsh..."
@@ -17,16 +22,18 @@ sudo apt install zsh -y
 
 # git install
 echo ''
-echo "Installing git and bash-completion..."
-sudo apt-get install git bash-completion -y
+echo "Installing git..."
+sudo apt-get install -y
 
-# Upgrade all the things
+# git useful defaults
 echo ''
-echo "Upgrade all apt packages and prune unsed ones..."
-sudo apt-get upgrade -y && sudo apt-get autoremove -y
+echo "Setting git config --global pull.rebase false"
+git config --global pull.rebase false
 
+# git-completion.bash
 echo ''
 echo "Configuring git-completion..."
+if [ -f "$HOME/.git-completion.bash" ]; then rm -Rf "$HOME/.git-completion.bash"; fi
 GIT_VERSION=`git --version | awk '{print $3}'`
 URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
 echo ''
@@ -35,23 +42,51 @@ if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
 	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
 fi
 
+# bash-completion install
+echo ''
+echo "Installing bash-completion..."
+sudo apt-get install bash-completion -y
+
 # oh-my-zsh install
-echo ''
-echo "Installing oh-my-zsh..."
-echo ''
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$ZSH" ]
+then
+    echo ''
+    echo "Installing oh-my-zsh..."
+    echo ''
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # oh-my-zsh plugin install
-echo ''
-echo "Installing oh-my-zsh plugins..."
-echo ''
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+if [ -d "~/.zsh/zsh-syntax-highlighting" ]
+then
+    echo ''
+    echo "Installing oh-my-zsh plugins..."
+    echo ''
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+else
+    echo ''
+    echo "Pulling oh-my-zsh plugins repo..."
+    pushd $(pwd)
+    cd ~/.zsh/zsh-syntax-highlighting && git pull
+    popd
+    echo ''
+fi
 
-# powerlevel9k install
-echo ''
-echo "Installing powerlevel9k..."
-echo ''
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# powerlevel10k
+if [ -d "~/.oh-my-zsh/custom/themes/powerlevel10k" ]
+then
+    echo ''
+    echo "Installing powerlevel10k..."
+    echo ''
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+else
+    echo ''
+    echo "Pulling powerlevel10k repo..."
+    pushd $(pwd)
+    cd ~/.oh-my-zsh/custom/themes/powerlevel10k && git pull
+    popd
+    echo ''
+fi
 
 # Bash color scheme
 echo ''
@@ -68,13 +103,18 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo ''
-echo "Now pulling down Gilles Zunino dotfiles..."
-git clone https://github.com/gilleszunino/dotfiles.git ~/.dotfiles
+echo "Installing Gilles Zunino dotfiles..."
+if [ -d "~/.dotfiles" ]
+then
+    git clone https://github.com/gilleszunino/dotfiles.git ~/.dotfiles
+fi
 echo ''
 pushd $(pwd) 
 cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
 echo ''
 echo "Checking out WSL branch..." && git checkout wsl
+echo ''
+echo "Pulling latest WSL branch..." && git pull
 echo ''
 echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
 if [[ $? -eq 0 ]]
@@ -94,7 +134,7 @@ echo ''
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "Setting zsh as default shell..."
-    chsh -s $(which zsh); exit 0
+    sudo chsh -s $(which zsh) $USER
     if [[ $? -eq 0 ]]
     then
         echo "Successfully set your default shell to zsh..."
@@ -104,5 +144,4 @@ fi
 else 
     echo "You chose not to configure zsh as default shell"
 fi
-echo ''
-echo '	Done'
+echo 'Done'
