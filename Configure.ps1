b561a8cf6f07b6a537142437037f1ca1cf390515
Gilles Zunino <GillesZunino@users.noreply.github.com>
2021-02-02 22:50:08 -0800

    Create Powershell Profile file if it does not yet exists

--- Configure.ps1
+++ Configure.ps1
@@ -18,5 +18,12 @@ Write-Host "Refreshing oh-my-posh theme at" $themeFullyQualifiedPath
 New-Item -ItemType Directory -Force -Path $userOhMyPoshProfileFullPath
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/GillesZunino/dotfiles/powershell/oh-my-posh/GillesIO.omp.json -OutFile $themeFullyQualifiedPath
 
+# Create profile if it does not exist
+if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts))
+{
+    Write-Host "Creating profile for Current User, All Hosts at " $PROFILE.CurrentUserAllHosts
+    New-Item -Type File -Path $PROFILE.CurrentUserAllHosts -Force
+}
+
 # Update $PROFILE to configure the custom theme
 # TODO: Automate adding to $PROFILE the following - Set-PoshPrompt "$env:USERPROFILE\oh-my-posh\GillesIO.omp.json"
