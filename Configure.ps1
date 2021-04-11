0dd2c99223927b55156a02e2e4750a8544ea868e
Gilles Zunino <GillesZunino@users.noreply.github.com>
2021-04-10 23:59:25 -0700

    Install and activate 'Terminal-Icons' - https://github.com/devblackops/Terminal-Icons

--- Configure.ps1
+++ Configure.ps1
@@ -79,7 +79,22 @@ function Install-OhMyPosh() {
 }
 
 
+function Install-TerminalIcons() {
+    [string] $TerminalIconsUserFriendlyName = "Terminal-Icons"
+
+    # Install Terminal-Icons
+    InstallOrRefresh-Module $TerminalIconsUserFriendlyName
+
+    # Update $PROFILE to configure the custom theme if it is not yet enabled
+    [string] $terminalIconsEnableCommand = "Import-Module -Name $TerminalIconsUserFriendlyName"
+    Append-ToCurrentUserAllHostsProfile "# Start $TerminalIconsUserFriendlyName`r`n$terminalIconsEnableCommand" $TerminalIconsUserFriendlyName
+}
+
+
 
 
 # Install oh-my-posh 3, copy custom theme(s) and insert entry in user profile
 Install-OhMyPosh
+
+# Install Terminal-Icons
+Install-TerminalIcons
