5f8e299227d5c78ec68abd74d78d00a2b1b51962
Gilles Zunino <GillesZunino@users.noreply.github.com>
2021-04-11 18:20:05 -0700

    Improve user feedback - Update utility function names

--- Install-OhMyPosh.psm1
+++ Install-OhMyPosh.psm1
@@ -4,7 +4,7 @@ function Install-OhMyPosh() {
     [string] $ohMyPoshThemeFileUrl = "https://raw.githubusercontent.com/GillesZunino/dotfiles/powershell/oh-my-posh/GillesIO.omp.json"
 
     # Install oh-my-posh
-    InstallOrRefresh-Module $ohMyPoshUserFriendlyName
+    Install-GalleryPackage $ohMyPoshUserFriendlyName
     
     # Copy custom oh-my-posh theme
     [string] $userOhMyPoshProfileFullPath = Join-Path -Path $env:USERPROFILE -ChildPath $ohMyPoshUserFriendlyName
@@ -12,16 +12,16 @@ function Install-OhMyPosh() {
 
     if (Test-Path -Path $themeFullyQualifiedPath) {
         Write-Host "Custom theme already exists - Removing"
-        Remove-Item -Path $themeFullyQualifiedPath -Force
+        Remove-Item -Path $themeFullyQualifiedPath -Force | Out-Null
     }
 
     Write-Host "Refreshing '$ohMyPoshUserFriendlyName' theme at '$themeFullyQualifiedPath'"
-    New-Item -ItemType Directory -Force -Path $userOhMyPoshProfileFullPath
+    New-Item -ItemType Directory -Force -Path $userOhMyPoshProfileFullPath | Out-Null
     Invoke-WebRequest -Uri $ohMyPoshThemeFileUrl -OutFile $themeFullyQualifiedPath
     
     # Update $PROFILE to configure the custom theme if it is not yet enabled
     [string] $ohMyPoshPromptEnableCommand = 'Set-PoshPrompt "$env:USERPROFILE\' + $ohMyPoshUserFriendlyName + '\' + $ohMyPoshThemeFileName + '"'
-    Append-ToCurrentUserAllHostsProfile "# Start $ohMyPoshUserFriendlyName`r`n$ohMyPoshPromptEnableCommand" $ohMyPoshUserFriendlyName
+    Merge-CommandToCurrentUserAllHostsProfile "# Start $ohMyPoshUserFriendlyName`r`n$ohMyPoshPromptEnableCommand" $ohMyPoshUserFriendlyName
 }
 
 Export-ModuleMember -Function Install-OhMyPosh
--- Install-TerminalIcons.psm1
+++ Install-TerminalIcons.psm1
@@ -2,11 +2,11 @@ function Install-TerminalIcons() {
     [string] $TerminalIconsUserFriendlyName = "Terminal-Icons"
 
     # Install Terminal-Icons
-    InstallOrRefresh-Module $TerminalIconsUserFriendlyName
+    Install-GalleryPackage $TerminalIconsUserFriendlyName
 
     # Update $PROFILE to configure the custom theme if it is not yet enabled
     [string] $terminalIconsEnableCommand = "Import-Module -Name $TerminalIconsUserFriendlyName"
-    Append-ToCurrentUserAllHostsProfile "# Start $TerminalIconsUserFriendlyName`r`n$terminalIconsEnableCommand" $TerminalIconsUserFriendlyName
+    Merge-CommandToCurrentUserAllHostsProfile "# Start $TerminalIconsUserFriendlyName`r`n$terminalIconsEnableCommand" $TerminalIconsUserFriendlyName
 }
 
 Export-ModuleMember -Function Install-TerminalIcons
