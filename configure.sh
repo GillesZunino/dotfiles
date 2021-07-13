ce8fb057f35a7d73fed9356feb4f3ecc14d0c738
Gilles Zunino <gzunino@hotmail.com>
2021-07-12 23:15:31 -0700

    Basic bootstrap and installation script

---
+++ configure.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# Upgrade all the things
+echo ''
+echo "Upgrade all apt packages and prune unused ones..."
+brew update && brew outdated && brew upgrade
+
+# git install
+echo ''
+echo "Installing git..."
+sbrew install git
+
+# git useful defaults
+echo ''
+echo "Setting git config --global pull.rebase false"
+git config --global pull.rebase false
+
+# git-completion.bash
+echo ''
+echo "Configuring git-completion..."
+if [ -f "$HOME/.git-completion.bash" ]; then rm -Rf "$HOME/.git-completion.bash"; fi
+GIT_VERSION=`git --version | awk '{print $3}'`
+URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
+echo ''
+echo "Downloading git-completion for git version: $GIT_VERSION..."
+if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
+	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
+fi
+
+# bash-completion install
+echo ''
+echo "Installing bash-completion..."
+sudo apt-get install bash-completion -y
+
+# oh-my-zsh install
+if [ ! -d "$ZSH" ]
+then
+    echo ''
+    echo "Installing oh-my-zsh..."
+    echo ''
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# oh-my-zsh plugin install
+if [ ! -d "~/.zsh/zsh-syntax-highlighting" ]
+then
+    echo ''
+    echo "Installing oh-my-zsh plugins..."
+    echo ''
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+else
+    echo ''
+    echo "Pulling oh-my-zsh plugins repo..."
+    pushd $(pwd)
+    cd ~/.zsh/zsh-syntax-highlighting && git pull
+    popd
+    echo ''
+fi
+
+# powerlevel10k
+if [ ! -d "~/.oh-my-zsh/custom/themes/powerlevel10k" ]
+then
+    echo ''
+    echo "Installing powerlevel10k..."
+    echo ''
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+else
+    echo ''
+    echo "Pulling powerlevel10k repo..."
+    pushd $(pwd)
+    cd ~/.oh-my-zsh/custom/themes/powerlevel10k && git pull
+    popd
+    echo ''
+fis
+
+# vim-plug install
+echo ''
+echo "Installing vim-plug..."
+echo ''
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo ''
+echo "Installing Gilles Zunino dotfiles..."
+if [ ! -d "~/.dotfiles" ]
+then
+    git clone https://github.com/gilleszunino/dotfiles.git ~/.dotfiles
+fi
+echo ''
+pushd $(pwd) 
+cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+echo ''
+echo "Checking out WSL branch..." && git checkout wsl
+echo ''
+echo "Pulling latest WSL branch..." && git reset --hard origin/wsl && git pull
+echo ''
+echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+if [[ $? -eq 0 ]]
+then
+    echo "Successfully configured your environment with Gilles Zunino's dotfiles..."
+else
+    echo "Gilles Zunino's dotfiles were not applied successfully..." >&2
+fi
+
+# Restore path
+popd
+
+# Set default shell to zsh only if it is not currently set
+currentShell=`grep $USER /etc/passwd | awk -F: '{ print $7 }'`
+if [ "$currentShell" != "/usr/bin/zsh" ]; then
+    echo ''
+    read -p "Do you want to change your default shell? Yy/Nn: " -n 1 -r REPLY
+    echo ''
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Setting zsh as default shell..."
+        sudo chsh -s $(which zsh) $USER
+        if [[ $? -eq 0 ]]
+        then
+            echo "Successfully set your default shell to zsh..."
+        else
+            echo "Default shell not set successfully..." >&2
+    fi
+    else 
+        echo "You chose not to configure zsh as default shell"
+    fi
+    echo 'Done'
+fi
