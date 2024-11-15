edba4b486be0fd1bd121492a71fbc94ccd4af86e
Gilles Zunino <GillesZunino@users.noreply.github.com>
2024-11-15 11:52:36 -0800

    Add powershell profile

---
+++ pwsh/profile.ps1
@@ -0,0 +1,5 @@
+# Start Terminal-Icons
+Import-Module Terminal-Icons
+
+# Start oh-my-posh
+/usr/local/bin/oh-my-posh init pwsh --config "~/.dotfiles/oh-my-posh/GillesIO.omp.json" | Invoke-Expression 
